cc484b5f52c1f1c8af08e46da45f1937a31a0677
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2021-01-07 20:08:28 +0500

    added bottomBar

--- lib/views/styles/styles-main.dart
+++ lib/views/styles/styles-main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:splyxp/widgets/InnerAppBar.dart';
 import 'package:splyxp/widgets/horizontalList.dart';
 import 'package:splyxp/widgets/lineHeading.dart';
+import 'package:splyxp/widgets/navbar.dart';
+import '../../views/search.dart';
+import '../../views/profile.dart';
+import '../../views/sply-network.dart';
+import '../../views/home.dart';
 
 class Styles extends StatefulWidget {
   @override
@@ -15,112 +20,146 @@ class _StylesState extends State<Styles> {
     'assets/images/styles/list3.jpg',
     'assets/images/styles/list4.jpg'
   ];
+  int index = 0;
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    if (_selectedIndex == 0 && index == 0) {
+      Navigator.of(context).pop();
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static List<Widget> _bottomNavList = [
+    Styles(),
+    Search(),
+    Home(),
+    SplyNetwork(),
+    Profile()
+  ];
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     print(width);
-    return Scaffold(
-      // App bar
-      appBar: Appbar(context),
+    return WillPopScope(
+        onWillPop: () async {
+          if (_selectedIndex == 0) return true;
+          setState(() {
+            _selectedIndex = 0;
+          });
+          return false;
+        },
+        child: Scaffold(
+          // App bar
+          appBar: Appbar(context),
 
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(right: 20, left: 20),
-              child: Image.asset(
-                'assets/images/styles/styles2.png',
-              ),
-            ),
-            Padding(
-              padding: width < 400
-                  ? EdgeInsets.only(top: 20.0, left: 41)
-                  : EdgeInsets.only(top: 20.0, left: 45),
-              child: Text(
-                  'Personalized style discovery and shopping as a service.\nWhere style meets fashion.',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400)),
-            ),
-            // Featured Style Heading
-            Heading(context, 'FEATURED STYLES'),
-            // Image and Rounded Card
-            Container(
-              padding: EdgeInsets.only(right: 20, left: 20),
-              child: Stack(children: [
-                Image.asset('assets/images/styles/styles4.jpg'),
-                RoundedCard(context, 0.97, 0.97),
-              ]),
-            ),
-            // Thome Brown Content
-            Padding(
-              padding: EdgeInsets.only(top: 35, left: 30, right: 30),
-              child: Text(
-                'Thome Brown Winter Clothing Styling By End Clothing',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 18, left: 32, right: 30),
-              child: Text(
-                'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
-                style: TextStyle(
-                    fontSize: 16,
-                    height: 1.5,
-                    color: Colors.grey[600],
-                    fontWeight: FontWeight.w400),
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            // horizontal Scrolling list
-            SizedBox(
-              height: 270,
-              child: horizontalListWith3(context, img),
-            ),
-            // spacing
-            SizedBox(
-              height: 50,
-            ),
-            // picture 2
-            Container(
-              color: Colors.grey[100],
-              padding: EdgeInsets.only(right: 20, left: 20),
-              child: Stack(children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 35.0, bottom: 35),
-                  child: Image.asset('assets/images/styles/style3.jpg'),
-                ),
-                RoundedCard(context, 0.9, 0.88)
-              ]),
-            ),
-            Container(
-              color: Colors.grey[100],
-              padding: EdgeInsets.only(top: 35, left: 30, right: 30),
-              child: Text(
-                'Thome Brown Winter Clothing Styling By End Clothing',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              color: Colors.grey[100],
-              padding:
-                  EdgeInsets.only(top: 18, left: 32, right: 30, bottom: 15),
-              child: Text(
-                'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
-                style: TextStyle(
-                    fontSize: 16,
-                    height: 1.5,
-                    color: Colors.grey[600],
-                    fontWeight: FontWeight.w400),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          body: _selectedIndex == 0
+              ? SingleChildScrollView(
+                  // key: new PageStorageKey('feed'),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right: 20, left: 20),
+                        child: Image.asset(
+                          'assets/images/styles/styles2.png',
+                        ),
+                      ),
+                      Padding(
+                        padding: width < 400
+                            ? EdgeInsets.only(top: 20.0, left: 41)
+                            : EdgeInsets.only(top: 20.0, left: 45),
+                        child: Text(
+                            'Personalized style discovery and shopping as a service.\nWhere style meets fashion.',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400)),
+                      ),
+                      // Featured Style Heading
+                      Heading(context, 'FEATURED STYLES'),
+                      // Image and Rounded Card
+                      Container(
+                        padding: EdgeInsets.only(right: 20, left: 20),
+                        child: Stack(children: [
+                          Image.asset('assets/images/styles/styles4.jpg'),
+                          RoundedCard(context, 0.97, 0.97),
+                        ]),
+                      ),
+                      // Thome Brown Content
+                      Padding(
+                        padding: EdgeInsets.only(top: 35, left: 30, right: 30),
+                        child: Text(
+                          'Thome Brown Winter Clothing Styling By End Clothing',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 18, left: 32, right: 30),
+                        child: Text(
+                          'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
+                          style: TextStyle(
+                              fontSize: 16,
+                              height: 1.5,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      // horizontal Scrolling list
+                      SizedBox(
+                        height: 270,
+                        child: horizontalListWith3(context, img),
+                      ),
+                      // spacing
+                      SizedBox(
+                        height: 50,
+                      ),
+                      // picture 2
+                      Container(
+                        color: Colors.grey[100],
+                        padding: EdgeInsets.only(right: 20, left: 20),
+                        child: Stack(children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 35.0, bottom: 35),
+                            child:
+                                Image.asset('assets/images/styles/style3.jpg'),
+                          ),
+                          RoundedCard(context, 0.9, 0.88)
+                        ]),
+                      ),
+                      Container(
+                        color: Colors.grey[100],
+                        padding: EdgeInsets.only(top: 35, left: 30, right: 30),
+                        child: Text(
+                          'Thome Brown Winter Clothing Styling By End Clothing',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.grey[100],
+                        padding: EdgeInsets.only(
+                            top: 18, left: 32, right: 30, bottom: 15),
+                        child: Text(
+                          'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
+                          style: TextStyle(
+                              fontSize: 16,
+                              height: 1.5,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : _bottomNavList.elementAt(_selectedIndex),
+          bottomNavigationBar: Navbar(_onItemTapped, _selectedIndex),
+        ));
   }
 }
 
