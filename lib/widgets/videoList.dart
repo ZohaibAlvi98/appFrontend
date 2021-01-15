f2e121b18a2fe73eea01e585383cf5a442d2bd3b
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2021-01-15 18:36:30 +0500

    added fixes

--- lib/widgets/videoList.dart
+++ lib/widgets/videoList.dart
@@ -1,5 +1,6 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoList extends StatefulWidget {
@@ -59,95 +60,141 @@ class _VideoListState extends State<VideoList> {
               child: GestureDetector(
                 onTap: () {
                   showDialog(
+                      barrierDismissible: false,
                       context: context,
                       builder: (BuildContext context) {
                         return AlertDialog(
-                          insetPadding: EdgeInsets.only(left: 8, right: 8),
+                          insetPadding: EdgeInsets.only(left: 3, right: 2),
                           backgroundColor: Colors.black,
-                          content: Stack(
-                            overflow: Overflow.visible,
-                            children: <Widget>[
-                              Positioned(
-                                right: -30.0,
-                                top: -40.0,
-                                child: InkResponse(
-                                  onTap: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: CircleAvatar(
-                                    maxRadius: 15,
+                          actions: [
+                            Stack(
+                              overflow: Overflow.visible,
+                              children: [
+                                Positioned(
+                                  child: MaterialButton(
+                                    height: 25,
+                                    color: Colors.red,
+                                    shape: CircleBorder(),
                                     child: Icon(
                                       Icons.close,
-                                      size: 18,
+                                      size: 14,
                                     ),
-                                    backgroundColor: Colors.red,
+                                    onPressed: () {
+                                      if (_controller == null) {
+                                      } else {
+                                        if (_controller.value.isPlaying ||
+                                            _controller.value.isBuffering ||
+                                            _controller.value.isLooping) {
+                                          _controller.pause();
+                                        }
+                                      }
+                                      // setState(() {
+                                      //   _controller = null;
+                                      // });
+
+                                      Navigator.of(context).pop();
+                                    },
                                   ),
                                 ),
-                              ),
-                              Container(
-                                width: 1000,
-                                height: 300,
-                                child: FutureBuilder(
-                                  future: _initializeVideoPlayerFuture,
-                                  builder: (context, snapshot) {
-                                    if (_controller == null) {
-                                      // If there was no controller, just create a new one
-                                      _initController(widget.videoUrl[index]);
-                                    } else {
-                                      final oldController = _controller;
+                              ],
+                            ),
+                            Container(
+                              width: 1000,
+                              height: 250,
+                              child: FutureBuilder(
+                                future: _initializeVideoPlayerFuture,
+                                builder: (context, snapshot) {
+                                  if (_controller == null) {
+                                    // If there was no controller, just create a new one
+                                    _initController(widget.videoUrl[index]);
+                                  } else {
+                                    final oldController = _controller;
 
-                                      // Registering a callback for the end of next frame
-                                      // to dispose of an old controller
-                                      // (which won't be used anymore after calling setState)
-                                      WidgetsBinding.instance
-                                          .addPostFrameCallback((_) async {
-                                        await oldController.dispose();
+                                    // Registering a callback for the end of next frame
+                                    // to dispose of an old controller
+                                    // (which won't be used anymore after calling setState)
+                                    WidgetsBinding.instance
+                                        .addPostFrameCallback((_) async {
+                                      await oldController.dispose();
 
-                                        // Initing new controller
-                                        // _initController(
-                                        //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
-                                      });
+                                      // Initing new controller
+                                      // _initController(
+                                      //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+                                    });
 
-                                      // Making sure that controller is not used by setting it to null
+                                    // Making sure that controller is not used by setting it to null
 
-                                      setState(() {
-                                        _controller = null;
-                                      });
-                                    }
-                                    return AspectRatio(
-                                      aspectRatio:
-                                          _controller.value.aspectRatio,
-                                      child: Chewie(
-                                        // key: new PageStorageKey(widget.url),
-                                        controller: ChewieController(
-                                          allowFullScreen: true,
-                                          allowPlaybackSpeedChanging: false,
-                                          videoPlayerController: _controller,
-                                          aspectRatio: 3 / 2,
-                                          // Prepare the video to be played and display the first frame
-                                          autoInitialize: true,
-                                          looping: false,
-                                          autoPlay: false,
-                                          // Errors can occur for example when trying to play a video
-                                          // from a non-existent URL
-                                          errorBuilder:
-                                              (context, errorMessage) {
-                                            return Center(
-                                              child: Text(
-                                                errorMessage,
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              ),
-                                            );
-                                          },
-                                        ),
+                                    setState(() {
+                                      _controller = null;
+                                    });
+                                  }
+                                  return AspectRatio(
+                                    aspectRatio: _controller.value.aspectRatio,
+                                    child: Chewie(
+                                      // key: new PageStorageKey(widget.url),
+                                      controller: ChewieController(
+                                        allowFullScreen: true,
+                                        allowPlaybackSpeedChanging: false,
+                                        videoPlayerController: _controller,
+                                        aspectRatio: 3 / 2,
+                                        // Prepare the video to be played and display the first frame
+                                        autoInitialize: true,
+                                        looping: false,
+                                        autoPlay: false,
+                                        // Errors can occur for example when trying to play a video
+                                        // from a non-existent URL
+                                        errorBuilder: (context, errorMessage) {
+                                          return Center(
+                                            child: Text(
+                                              errorMessage,
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          );
+                                        },
                                       ),
-                                    );
-                                  },
-                                ),
-                              )
-                            ],
-                          ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                          // content: Stack(
+                          //   overflow: Overflow.visible,
+                          //   children: <Widget>[
+                          //     Positioned(
+                          //       right: -30.0,
+                          //       top: -40.0,
+                          //       child: InkResponse(
+                          //         onTap: () {
+                          //           final oldController = _controller;
+
+                          //           // Registering a callback for the end of next frame
+                          //           // to dispose of an old controller
+                          //           // (which won't be used anymore after calling setState)
+                          //           WidgetsBinding.instance
+                          //               .addPostFrameCallback((_) async {
+                          //             await oldController.dispose();
+
+                          //             // Initing new controller
+                          //             // _initController(
+                          //             //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+                          //           });
+                          //           setState(() {
+                          //             _controller = null;
+                          //           });
+                          //           Navigator.of(context).pop();
+                          //         },
+                          //         child: CircleAvatar(
+                          //           maxRadius: 15,
+                          //           child: Icon(
+                          //             Icons.close,
+                          //             size: 18,
+                          //           ),
+                          //           backgroundColor: Colors.red,
+                          //         ),
+                          //       ),
+                          //     ),
                         );
                       });
                 },
