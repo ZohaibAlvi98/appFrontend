977d27e87ca9ed14cdec268cf65594498396259e
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2021-03-03 20:14:52 +0500

    working on channel

--- lib/views/s-plyrs/channel/channel.dart
+++ lib/views/s-plyrs/channel/channel.dart
@@ -7,6 +7,62 @@ class Channel extends StatefulWidget {
 }
 
 class _ChannelState extends State<Channel> {
+  // List<Widget> containers = [
+  //   SizedBox(
+  //     height: 100,
+  //     child: ListView.builder(
+  //         scrollDirection: Axis.vertical,
+  //         shrinkWrap: true,
+  //         itemCount: 12,
+  //         physics: AlwaysScrollableScrollPhysics(),
+  //         padding: EdgeInsets.only(right: 20.0, left: 7, top: 20),
+  //         itemBuilder: (context, index) {
+  //           List women = [
+  //             'New In',
+  //             'Shop By',
+  //             'Clothing',
+  //             'Bags',
+  //             'Jewelry',
+  //             'Accessories',
+  //             'Item 1',
+  //             'Item 2',
+  //             'Item 3',
+  //             'Item 4',
+  //             'Item 5',
+  //             'Item 6',
+  //           ];
+  //           double width = MediaQuery.of(context).size.width;
+  //           return Container();
+  //         }),
+  //   ),
+  //   SizedBox(
+  //     height: 100,
+  //     child: ListView.builder(
+  //         scrollDirection: Axis.vertical,
+  //         shrinkWrap: true,
+  //         itemCount: 12,
+  //         physics: AlwaysScrollableScrollPhysics(),
+  //         padding: EdgeInsets.only(right: 20.0, left: 7, top: 20),
+  //         itemBuilder: (context, index) {
+  //           List men = [
+  //             'Shop By',
+  //             'Clothing',
+  //             'Activewear',
+  //             'Shoes',
+  //             'Sneakers',
+  //             'Watches',
+  //             'Item 1',
+  //             'Item 2',
+  //             'Item 3',
+  //             'Item 4',
+  //             'Item 5',
+  //             'Item 6',
+  //           ];
+  //           double width = MediaQuery.of(context).size.width;
+  //           return Container();
+  //         }),
+  //   ),
+  // ];
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -25,10 +81,10 @@ class _ChannelState extends State<Channel> {
                         fit: BoxFit.contain,
                       ),
                       Center(
-                        heightFactor: 2.2,
+                        heightFactor: 2.12,
                         child: Container(
-                          width: 180,
-                          height: 135,
+                          width: width / 2.71,
+                          height: width / 3,
                           decoration: new BoxDecoration(
                               border: new Border.all(
                                 color: Colors.grey[200],
@@ -49,7 +105,7 @@ class _ChannelState extends State<Channel> {
                             child: Text(
                               'S-PLY',
                               style: TextStyle(
-                                  fontSize: width < 400 ? 35 : 41,
+                                  fontSize: width < 400 ? 34 : 41,
                                   color: Colors.black,
                                   fontWeight: FontWeight.bold,
                                   letterSpacing: 1.2),
@@ -59,7 +115,7 @@ class _ChannelState extends State<Channel> {
                       ),
                       Center(
                         child: Padding(
-                          padding: EdgeInsets.only(top: 225.0),
+                          padding: EdgeInsets.only(top: width < 400 ? 210 : 225.0),
                           child: Text(
                             'S-PLY STORE',
                             style: TextStyle(
@@ -73,16 +129,6 @@ class _ChannelState extends State<Channel> {
                         padding: EdgeInsets.only(top: 270),
                         child: Column(
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 50, right: 50),
-                              child: Text(
-                                'S-PLY is an online and retail concept store founded in London, with planned locations in Amsterdam, New York and Los Angeles',
-                                style: TextStyle(fontSize: 17, height: 1.3),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 18,
-                            ),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
@@ -93,9 +139,9 @@ class _ChannelState extends State<Channel> {
                                   ),
                                 ),
                                 Padding(
-                                  padding: EdgeInsets.only(right: 10.0),
+                                  padding: EdgeInsets.only(right: 18.0),
                                   child: Text(
-                                    'S-PLY Store, London, United Kingdom(UK)',
+                                    'S-PLY Store, London, United Kingdom',
                                     style: TextStyle(fontSize: 15.5),
                                   ),
                                 ),
@@ -105,10 +151,10 @@ class _ChannelState extends State<Channel> {
                               height: 7,
                             ),
                             Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Padding(
-                                  padding: EdgeInsets.only(left: 45.0),
+                                  padding: EdgeInsets.only(left: 0.0),
                                   child: Icon(
                                     Icons.mail_sharp,
                                   ),
@@ -121,13 +167,131 @@ class _ChannelState extends State<Channel> {
                             ),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        side: BorderSide(color: Colors.black87, width: 1.4)),
+                    onPressed: () {
+                      // _navigatorPage('request');
+                    },
+                    child: Text(
+                      'SUBSCRIBE NOW',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    color: Colors.white,
+                    minWidth: 200,
+                    height: 54,
+                  ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 14, bottom: 10),
+                  child: Divider(
+                    thickness: 2,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15, right: 14),
+                  child: Text(
+                    'S-PLY is an online and retail concept store founded in London, with planned locations in Amsterdam, New York and Los Angeles',
+                    style: TextStyle(fontSize: 17, height: 1.3),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                story(context),
+                SizedBox(
+                  height: 20,
                 ),
               ],
             ),
           ),
         ));
   }
+}
+
+Widget story(context) {
+  final stories = Expanded(
+    child: Padding(
+      padding: EdgeInsets.only(top: 2.0),
+      child: new ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (context, index) => new Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  Container(
+                    width: 80,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage(
+                                'assets/images/sneakers/list2.jpg'))),
+                    margin: EdgeInsets.symmetric(horizontal: 7),
+                  )
+                ],
+              )),
+    ),
+  );
+
+  var deviceSize = MediaQuery.of(context).size;
+
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      SizedBox(
+        height: deviceSize.height * 0.2,
+        child: Flexible(
+            child: new ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 1,
+                itemBuilder: (context, index) => index == 0
+                    ? new SizedBox(
+                        height: 180,
+                        child: Container(
+                          margin: EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Stories',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                ],
+                              ),
+                              stories,
+                              SizedBox(
+                                height: 25,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    : Column())),
+      )
+    ],
+  );
 }
