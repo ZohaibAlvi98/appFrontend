60d6eaa3a55b38dd6ea78c30c48ed4b7e5223821
wasiqhussain97 <wasiq.smaztech@gmail.com>
2021-04-09 17:37:51 -0700

    rss section api listing

--- lib/views/sply-network.dart
+++ lib/views/sply-network.dart
@@ -7,6 +7,8 @@ class SplyNetwork extends StatefulWidget {
   _SplyNetworkState createState() => _SplyNetworkState();
 }
 
+Editorials data = new Editorials();
+
 class _SplyNetworkState extends State<SplyNetwork> {
   @override
   Widget build(BuildContext context) {
@@ -15,15 +17,55 @@ class _SplyNetworkState extends State<SplyNetwork> {
       child: Container(
         child: Column(
           children: [
-            cards(width, 'assets/images/rss/rss1.jpg', context),
+            // cards(width, 'assets/images/rss/rss1.jpg', context),
+            FutureBuilder(
+                future: data.getEditorials(),
+                // artistService.getArtist(page),
+
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                  if (snapshot.hasData) {
+                    // List<ArtistModel> artist = snapshot.data;
+                    return GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: EdgeInsets.only(left: 0, right: 0, top: 0),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          childAspectRatio: MediaQuery.of(context).size.width /
+                              (MediaQuery.of(context).size.height / 1.85),
+                        ),
+                        // scrollDirection: Axis.vertical,
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final item = snapshot.data[index];
+                          return editirialscards(
+                            width,
+                            context,
+                            // 'men',
+                            item['image'],
+                            index,
+                            item['title'],
+                            item['date'],
+                            item['author'],
+                            item['author_logo'],
+                            item['short_text'],
+                          );
+                        });
+                  } else if (snapshot.hasError) {
+                    print(snapshot.error);
+                    print('Sorry');
+                  }
+                  return Center(child: CircularProgressIndicator());
+                }),
             SizedBox(
               height: 15,
             ),
-            cards(width, 'assets/images/rss/rss2.jpg', context),
-            SizedBox(
-              height: 15,
-            ),
-            cards(width, 'assets/images/rss/rss1.jpg', context),
+            // cards(width, 'assets/images/rss/rss2.jpg', context),
+            // SizedBox(
+            //   height: 15,
+            // ),
+            // cards(width, 'assets/images/rss/rss1.jpg', context),
           ],
         ),
       ),
@@ -150,6 +192,160 @@ Widget cards(width, img, context) {
                           child: Text(
                             'Lorem ipsum is a placeholder text commonly used to demonstrate the visual document...',
                             style: TextStyle(height: 1.3),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ));
+}
+
+Widget editirialscards(
+    width, context, image, index, title, date, author, authorlogo, shorttext) {
+  void _navigatorPage(index) {
+    // Navigator.of(context).pop(new PageRouteBuilder());
+    Navigator.of(context).push(new PageRouteBuilder(
+        opaque: true,
+        transitionDuration: const Duration(),
+        pageBuilder: (BuildContext context, _, __) {
+          return RssDetail();
+        },
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+          return new SlideTransition(
+            child: child,
+            position: new Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+          );
+        }));
+  }
+
+  final logo = "https:" + authorlogo;
+  return Container(
+      padding: EdgeInsets.only(right: 15, left: 15, top: 0),
+      child: InkWell(
+        onTap: () {
+          _navigatorPage(context);
+        },
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 7.0),
+          child: Card(
+            elevation: 1,
+            margin: EdgeInsets.only(bottom: 5),
+            child: Stack(
+              children: [
+                Image.network(
+                  image,
+                  height: 270,
+                  fit: BoxFit.contain,
+                ),
+                Align(
+                  // alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 180, left: 5),
+                        width: width * 0.14,
+                        height: 50,
+                        // decoration: new BoxDecoration(
+                        //   border: new Border.all(
+                        //     color: Colors.grey[400],
+                        //     width: 2,
+                        //   ),
+                        //   color: Colors.white,
+                        //   shape: BoxShape.circle,
+                        // ),
+
+                        // boxShadow: [
+                        //   BoxShadow(
+                        //     color: Colors.grey.withOpacity(0.8),
+                        //     spreadRadius: 0.1,
+                        //     blurRadius: 5,
+                        //     offset:
+                        //         Offset(0, 7), // changes position of shadow
+                        //   ),
+                        // ],
+                        // ),
+
+                        child: Padding(
+                          padding: width < 400
+                              ? EdgeInsets.only(left: 6.5, top: 10)
+                              : EdgeInsets.only(left: 6.0, top: 1),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(logo),
+                              radius: 30,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 285.0, left: 7),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: width < 400
+                                    ? EdgeInsets.only(right: 50)
+                                    : EdgeInsets.only(right: 50.0),
+                                child: Text(
+                                  author,
+                                  style: TextStyle(
+                                      fontSize: width < 400 ? 18 : 20,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 0.0),
+                                child: Text(
+                                  date,
+                                  style: TextStyle(
+                                      fontSize: width < 400 ? 11 : 14),
+                                ),
+                              )
+                            ],
+                          )),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 3),
+                  child: Column(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 13.0, top: 335, right: 14),
+                            child: Text(
+                              title,
+                              maxLines: 2,
+                              style: TextStyle(
+                                  fontSize: width < 400 ? 22 : 17,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black),
+                            ),
+                          )),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: 7.0, left: 13, bottom: 10, right: 13),
+                          child: SizedBox(
+                            width: 500,
+                            child: Text(
+                              shorttext,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontSize: 12),
+                            ),
                           ),
                         ),
                       )
