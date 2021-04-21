786a9ffce5c7b8a13e2c85e3b08acbc1ba6e4c7d
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2021-04-21 13:23:14 +0500

    homepage design and product images in details added

---
+++ lib/widgets/homescreentextpicture.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenButtonTextPicture extends StatelessWidget {
+  final String boldText;
+  final String infotext;
+  final String imgPath;
+  final Function onPressed;
+
+  HomeScreenButtonTextPicture(
+      {this.boldText, this.infotext, this.imgPath, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        margin: EdgeInsets.only(top: 4.0),
+        color: Colors.grey[200],
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(right: 20.0, top: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      boldText,
+                      style: TextStyle(
+                        fontFamily: 'RMNUEU',
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Text(
+                      infotext,
+                      style: TextStyle(
+                        fontFamily: 'RMNUEUREGULAR',
+                        fontSize: 15.0,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Image.asset(imgPath),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
